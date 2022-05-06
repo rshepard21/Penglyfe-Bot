a9a49811ac2c0397e6bc62ac3fe828696efbbe95
Ryan Shepard <30640731+rshepard21@users.noreply.github.com>
2022-05-05 21:47:59 -0500

    Update penglyfe.py
    Added new documentation.

--- penglyfe.py
+++ penglyfe.py
@@ -1,8 +1,10 @@
 """
-Adi before starting this bot, please use pip install discord.py, it is the needed package to run this file.
+Please use pip install discord.py, it is the needed package to run this file.
 After installed create a new empty tmux session and run this file with python penglyfe.py
 
 :) bot ready.
+
+You are free to do whatever you wish with this bot as i no longer maintain it.
 """
 import discord
 
@@ -16,11 +18,14 @@ class Penglyfe(discord.Client):
 	async def on_ready(self):
 		print('Logged in as {0}'.format(self.user))
 
+	"""
+	Detects when a player would have the penglyfe client open and post to a channel that said user logged into penglyfe.
+	"""
 	async def on_member_update(self, before, after):
 		game_before = [i for i in before.activities if str(i.type) == "ActivityType.playing" and str(i.name) == "Penglyfe"]
 		game_after = [i for i in after.activities if str(i.type) == "ActivityType.playing" and str(i.name) == "Penglyfe"]
 
-		channel = self.get_channel(885388958450724864)
+		channel = self.get_channel(# Channel id)
 		if game_before:
 			return
 		elif game_after:
